f3bffc9a3d38bd67647ab8aadd836f9541728ddb
Konstantinos Semertzidis <ksemer@cs.uoi.gr>
2016-10-20 13:23:52 +0300

    Update and rename create_dblp_graph.java to CreateDBLPGraph.java
    Support graph creation from specific conferences

--- src/CreateDBLPGraph.java
+++ src/CreateDBLPGraph.java
@@ -3,27 +3,34 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Creates the DBLP graph author1 \t author 2 \t time
  * 
  * @author ksemer
  */
-public class create_dblp_graph {
-	private String PATH_DBLP_INPUT = "dblp.txt";
-	private String PATH_DBLP_AUTHORS_MAP = "dblp_authors_ids";
-	private String PATH_DBLP_GRAPH = "dblp_graph";
-	private String PATH_DBLP_AUTHORS_CONFERENCES = "dblp_authors_conf";
+public class CreateDBLPGraph {
+	private final String PATH_DBLP_INPUT = "dblp.txt";
+	private final String PATH_DBLP_AUTHORS_MAP = "dblp_authors_ids";
+	private final String PATH_DBLP_GRAPH = "dblp_graph";
+	private final String PATH_DBLP_AUTHORS_CONFERENCES = "dblp_authors_conf";
 	
+	// Create graph from specific conferences
+	private final Set<String> CONFERENCES = new HashSet<String>(Arrays.asList("ICDE", "VLDB", "EDBT",
+			"SIGMOD Conference", "KDD", "KDD Cup", "WWW", "SIGIR", "CIKM", "SDM", "ICDM", "WWW (Companion Volume)"));
+
 	// is used to replace authors names with a unique id.
 	private HashMap<String, Integer> allAuthors = new HashMap<String, Integer>();
 	private FileWriter w_graph = new FileWriter(PATH_DBLP_GRAPH, false);
 	private FileWriter w_authors = new FileWriter(PATH_DBLP_AUTHORS_MAP, false);
 	private FileWriter w_authors_conf = new FileWriter(PATH_DBLP_AUTHORS_CONFERENCES, false);
 
-	public create_dblp_graph() throws IOException {
+	public CreateDBLPGraph() throws IOException {
 		BufferedReader input = new BufferedReader(new FileReader(PATH_DBLP_INPUT));
 		int year = 0, id = 0;
 		String line = null, booktitle = null, title = null;
@@ -65,14 +72,17 @@ public class create_dblp_graph {
 		System.out.println("DBLP graph is generated!");
 	}
 
-	
+	/**
+	 * Write dblp graph and authors conferences to files
+	 * @param booktitle
+	 * @param title
+	 * @param year
+	 * @param authors
+	 * @throws IOException
+	 */
 	private void writeF(String booktitle, String title, int year, List<String> authors) throws IOException {
 		// Used to generate graph from specific conferences
-		// if (title != null && (booktitle.equalsIgnoreCase("ICDE") || booktitle.equalsIgnoreCase("VLDB")
-				// || booktitle.equals("EDBT") || booktitle.equalsIgnoreCase("SIGMOD Conference")
-				// || booktitle.equals("KDD") || booktitle.equals("KDD Cup") || booktitle.equals("WWW")
-				// || booktitle.equals("WWW (Companion Volume)") || booktitle.equals("SIGIR") || booktitle.equals("CIKM")
-				// || booktitle.equals("ICDM") || booktitle.equals("SDM"))) {
+		if (title != null && (CONFERENCES.isEmpty() || CONFERENCES.contains(booktitle))) {
 
 			// Write the authors
 			for (int i = 0; i < authors.size(); i++) {
@@ -84,10 +94,10 @@ public class create_dblp_graph {
 					w_graph.write(authorA + "\t" + allAuthors.get(authors.get(j)) + "\t" + year + "\n");
 				}
 			}
-		// }
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
-		new create_dblp_graph();
+		new CreateDBLPGraph();
 	}
 }
